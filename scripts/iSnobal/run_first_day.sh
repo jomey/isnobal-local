4a51c49d0f13a25100832f19b4802b7fa9bf2243
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2025-01-16 16:44:27 -0700

    scripts - iSnobal - update env detection
    temp update to isnoda_py39 and remove load_conda_env line

--- scripts/iSnobal/run_first_day.sh
+++ scripts/iSnobal/run_first_day.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 #
 # Run for given day. Date format: 2017-10-01 00:00
-if [[ -n "$CONDA_DEFAULT_ENV" && "$CONDA_DEFAULT_ENV" == "isnoda" ]]; then
-  echo "Conda environment 'isnoda' is active."
+if [[ -n "$CONDA_DEFAULT_ENV" && "$CONDA_DEFAULT_ENV" == "isnoda_py39" ]]; then
+  echo "Conda environment 'isnoda_py39' is active."
 else
-  echo "Conda environment 'isnoda' is not active, exiting script"
+  echo "Conda environment 'isnoda_py39' is not active, exiting script"
   exit 1
 fi
-#source load_conda_env isnoda
 
 # Initial run, no previous days
 awsm_daily_airflow -c $1 \
